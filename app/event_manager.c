cc848b41ac4bc831ebe2832509cbc38482757fe5
valcheb <cheb.valentin@gmail.com>
2022-12-19 10:48:54 +0300

    refactor(event_manager): put static data in context struct

--- app/event_manager.c
+++ app/event_manager.c
@@ -1,13 +1,19 @@
 #include "event_manager.h"
 #include "event_ring.h"
 #include <stddef.h>
+#include <string.h>
 
 #define EVENT_DICTIONARY_SIZE 10
 #define EVENT_RING_SIZE 10
 
-static em_pair_t  em_dictionary[EVENT_DICTIONARY_SIZE];
-static em_ring_t  em_ring;
-static em_relem_t em_ring_buffer[EVENT_RING_SIZE];
+static struct
+{
+    em_pair_t  em_dictionary[EVENT_DICTIONARY_SIZE];
+    em_ring_t  em_ring;
+    em_relem_t em_ring_buffer[EVENT_RING_SIZE];
+    uint32_t   search_idx;
+    uint32_t   dict_curr_size;
+} em_ctx;
 
 /**
  * @brief Search index of event in dictionary.
@@ -20,7 +26,7 @@ static uint32_t em_find(em_event_t search_name, uint32_t search_idx)
 {
     while (search_idx < EVENT_DICTIONARY_SIZE)
     {
-        if (em_dictionary[search_idx].event == search_name)
+        if (em_ctx.em_dictionary[search_idx].event == search_name)
         {
             break;
         }
@@ -36,8 +42,8 @@ static bool em_is_pair_exist(em_event_t event, em_callback_t callback)
 
     while (idx < EVENT_DICTIONARY_SIZE)
     {
-        if (event    == em_dictionary[idx].event &&
-            callback == em_dictionary[idx].callback)
+        if (event    == em_ctx.em_dictionary[idx].event &&
+            callback == em_ctx.em_dictionary[idx].callback)
         {
             return true;
         }
@@ -50,14 +56,14 @@ static bool em_is_pair_exist(em_event_t event, em_callback_t callback)
 
 void em_init(void)
 {
-    er_init(&em_ring, em_ring_buffer, EVENT_RING_SIZE);
+    er_init(&em_ctx.em_ring, em_ctx.em_ring_buffer, EVENT_RING_SIZE);
+    em_ctx.search_idx     = 0;
+    em_ctx.dict_curr_size = 0;
 }
 
 void em_subscribe(em_event_t event, em_callback_t callback)
 {
-    static uint32_t dict_curr_size = 0;
-
-    if (dict_curr_size >= EVENT_DICTIONARY_SIZE)
+    if (em_ctx.dict_curr_size >= EVENT_DICTIONARY_SIZE)
     {
         //TODO how to message errors?
         return ;
@@ -68,46 +74,45 @@ void em_subscribe(em_event_t event, em_callback_t callback)
         return ;
     }
 
-    em_dictionary[dict_curr_size].event    = event;
-    em_dictionary[dict_curr_size].callback = callback;
-    dict_curr_size++;
+    em_ctx.em_dictionary[em_ctx.dict_curr_size].event    = event;
+    em_ctx.em_dictionary[em_ctx.dict_curr_size].callback = callback;
+    em_ctx.dict_curr_size++;
 }
 
 void em_emit(em_event_t event, em_arg_t arg)
 {
     em_relem_t elem = {.name = event, .arg = arg};
 
-    if (er_is_full(&em_ring))
+    if (er_is_full(&em_ctx.em_ring))
     {
         return ;
     }
 
-    er_push(&em_ring, elem);
+    er_push(&em_ctx.em_ring, elem);
 }
 
 void em_handler(void)
 {
-    static uint32_t search_idx = 0;
     static em_relem_t elem = {.name = EM_EVENT_UNDEFINED};
 
-    if (elem.name == EM_EVENT_UNDEFINED && !er_is_empty(&em_ring))
+    if (elem.name == EM_EVENT_UNDEFINED && !er_is_empty(&em_ctx.em_ring))
     {
-        er_pop(&em_ring, &elem);
+        er_pop(&em_ctx.em_ring, &elem);
     }
 
-    search_idx = em_find(elem.name, search_idx);
-    if (search_idx < EVENT_DICTIONARY_SIZE)
+    em_ctx.search_idx = em_find(elem.name, em_ctx.search_idx);
+    if (em_ctx.search_idx < EVENT_DICTIONARY_SIZE)
     {
-        if (em_dictionary[search_idx].callback != NULL)
+        if (em_ctx.em_dictionary[em_ctx.search_idx].callback != NULL)
         {
-            em_dictionary[search_idx].callback(elem.arg);
+            em_ctx.em_dictionary[em_ctx.search_idx].callback(elem.arg);
         }
 
-        search_idx++;
+        em_ctx.search_idx++;
     }
     else
     {
-        search_idx = 0;
-        elem.name  = EM_EVENT_UNDEFINED;
+        em_ctx.search_idx = 0;
+        elem.name         = EM_EVENT_UNDEFINED;
     }
 }
