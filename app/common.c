a592cdce45f2b519919884e1c47fb5d8fb89410f
valcheb <cheb.valentin@gmail.com>
2022-12-22 12:27:58 +0300

    feat(common): add player_turn_pos

--- app/common.c
+++ app/common.c
@@ -2,6 +2,7 @@
 
 static uint8_t cell_length;
 static point_t field_start;
+static point_t player_turn_pos;
 
 void cm_set_cell_length(uint16_t length)
 {
@@ -22,4 +23,15 @@ void cm_set_field_start(uint16_t x, uint16_t y)
 point_t cm_get_field_start(void)
 {
     return field_start;
+}
+
+void cm_set_player_turn_pos(uint16_t x, uint16_t y)
+{
+    player_turn_pos.x = x;
+    player_turn_pos.y = y;
+}
+
+point_t cm_get_player_turn_pos(void)
+{
+    return player_turn_pos;
 }
--- app/common.h
+++ app/common.h
@@ -21,5 +21,7 @@ void cm_set_cell_length(uint16_t length);
 uint8_t cm_get_cell_length(void);
 void cm_set_field_start(uint16_t x, uint16_t y);
 point_t cm_get_field_start(void);
+void cm_set_player_turn_pos(uint16_t x, uint16_t y);
+point_t cm_get_player_turn_pos(void);
 
 #endif /*COMMON_H_*/
