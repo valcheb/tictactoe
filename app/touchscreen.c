16f2ee74c2041783f49523f0824c1ba8a705f34a
valcheb <cheb.valentin@gmail.com>
2022-12-22 16:34:19 +0300

    fix(touchscreen): delete debug info

--- app/touchscreen.c
+++ app/touchscreen.c
@@ -1,5 +1,4 @@
 #include "touchscreen.h"
-#include "display.h"
 
 static TS_StateTypeDef TS_State;
 
@@ -17,22 +16,10 @@ void EXTI15_10_IRQHandler(void)
     if ((__HAL_GPIO_EXTI_GET_IT(TS_INT_PIN) >> 13) == SET)
     {
         //TODO avoid double tapping
-        //do something
         BSP_TS_GetState(&TS_State);
         if (TS_State.touchDetected)
         {
-            BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-            char str[13];
-            memset(str, 0, 13);
-            strcat(str, "X = ");
-            char ch[2];
-            utoa(TS_State.touchX[0], ch, 10);
-            strcat(str, ch);
-            strcat(str, ", Y = ");
-            utoa(TS_State.touchY[0], ch, 10);
-            strcat(str, ch);
-            BSP_LCD_ClearStringLine(3);
-            BSP_LCD_DisplayStringAtLine(3, (uint8_t*)str);
+            //do something
         }
         __HAL_GPIO_EXTI_CLEAR_IT(TS_INT_PIN);
     }
