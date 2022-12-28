032b891be4cb4123747da0a3a65b3cfe402a5564
valcheb <cheb.valentin@gmail.com>
2022-12-28 16:14:40 +0300

    feat(main): rewrite for screens support

--- main.c
+++ main.c
@@ -1,89 +1,76 @@
 #include "mcu.h"
 #include "display.h"
 #include "touchscreen.h"
+#include "game_screen.h"
+#include "title_screen.h"
 #include "event_manager.h"
 #include "ttt.h"
 
-static void led_on(void)
+void screen_title(const em_arg_t *in)
 {
-    HAL_GPIO_WritePin(GPIOI, GPIO_PIN_1, GPIO_PIN_SET);
+    em_unsubscribe(EM_EVENT_PRESS, gs_press_control);
+    em_subscribe(EM_EVENT_PRESS, title_press_control);
+
+    title_greetings_view(NULL); //tictactoe, local button, net button
 }
 
-static void led_off(void)
+void screen_game(const em_arg_t *in) //maybe in will contain first player
 {
-    HAL_GPIO_WritePin(GPIOI, GPIO_PIN_1, GPIO_PIN_RESET);
+    em_unsubscribe(EM_EVENT_PRESS, title_press_control);
+    em_subscribe(EM_EVENT_PRESS, gs_press_control);
+
+    gs_init_view();
 }
 
-static void setup_led(void)
+void local_game(const em_arg_t *in)
 {
-    GPIO_InitTypeDef gpioInitStructure;
+    //ttt_local_init();
+    ttt_init(NULL);
+    //em_unsubscribe(EM_EVENT_PLAY_AGAIN_PRESSED, net_game);
+    em_subscribe(EM_EVENT_PLAY_AGAIN_PRESSED, local_game);
+    em_subscribe(EM_EVENT_CELL_CALCULATED, ttt_handler);
+    screen_game(NULL);
+}
 
-    __HAL_RCC_GPIOI_CLK_ENABLE();
-    gpioInitStructure.Pin = GPIO_PIN_1;
-    gpioInitStructure.Mode = GPIO_MODE_OUTPUT_PP;
-    gpioInitStructure.Pull = GPIO_PULLUP;
-    gpioInitStructure.Speed = GPIO_SPEED_HIGH;
-    HAL_GPIO_Init(GPIOI, &gpioInitStructure);
-    led_off();
+void net_game(const em_arg_t *in)
+{
+    //ttt_net_init();
+    //em_unsubscribe(PLAY_AGAIN, local_game);
+    //em_subscribe(PLAY_AGAIN, net_game);
+    screen_game(NULL);
+}
+
+void on_boot(void)
+{
+    //init view and control elements
+    title_screen_init();
+    game_screen_init();
+
+    //subscribe common events
+    em_subscribe(EM_EVENT_LOCAL_PRESSED, local_game);
+    //em_subscribe(EM_EVENT_NET_PRESSED, net_game);
+    em_subscribe(EM_EVENT_NEW_TURN, gs_player_view);
+    em_subscribe(EM_EVENT_PLAYER_TURN, gs_turn_view);
+    em_subscribe(EM_EVENT_END_OF_GAME, gs_winner_view);
+    em_subscribe(EM_EVENT_MAIN_MENU_PRESSED, screen_title);
+    em_subscribe(EM_EVENT_BOOTED, screen_title);
+
+    em_emit(EM_EVENT_BOOTED, NULL);
 }
 
 int main(void)
 {
-    uint32_t i = 0;
-
     mcu_init();
     disp_init();
     ts_init();
     em_init();
 
-    em_subscribe(EM_EVENT_PRESS, ts_press_handler);
-    em_subscribe(EM_EVENT_CELL_CALCULATED, ttt_handler);
-    em_subscribe(EM_EVENT_PLAYER_TURN, disp_turn);
-    em_subscribe(EM_EVENT_INIT, ttt_init);
-    em_subscribe(EM_EVENT_NEW_TURN, disp_player);
-
-    disp_clean_field();
-    em_emit(EM_EVENT_INIT, NULL);
+    on_boot();
 
     while(1)
     {
         em_handler();
     }
 
-    /*disp_clean_field();
-
-    player_e cells[9] =
-    {
-        PLAYER_1, PLAYER_2, PLAYER_2,
-        PLAYER_2, PLAYER_1, PLAYER_1,
-        PLAYER_1, PLAYER_2, PLAYER_1
-    };
-
-    for (int i = 0; i < 9; i++)
-    {
-        disp_turn(cells[i], i);
-    }*/
-
-    /*setup_led();
-    while (1)
-    {
-        if (i % 2)
-        {
-            led_off();
-            BSP_LCD_SetTextColor(LCD_COLOR_RED);
-            BSP_LCD_DisplayStringAtLine(5, (uint8_t*)"Sample Text!");
-        }
-        else
-        {
-            led_on();
-            BSP_LCD_SetTextColor(LCD_COLOR_BLUE);
-            BSP_LCD_DisplayStringAtLine(5, (uint8_t*)"Sample Text!");
-        }
-
-        HAL_Delay(1000);
-        ++i;
-    }*/
-
-    /* NotReached */
     return 0;
 }
