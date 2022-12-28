ae60d3f64bd73200abec0a467eb4f5f9f4105bd0
valcheb <cheb.valentin@gmail.com>
2022-12-28 13:52:19 +0300

    feat(touchscreen): add double tap prevention

--- app/touchscreen.c
+++ app/touchscreen.c
@@ -5,6 +5,8 @@
 #include "stm32f7xx.h"
 #include "stm32746g_discovery_ts.h"
 
+#define DOUBLE_TAP_PAUSE 50
+
 static TS_StateTypeDef TS_State;
 
 void ts_init(void)
@@ -15,22 +17,25 @@ void ts_init(void)
 
 void EXTI15_10_IRQHandler(void)
 {
+    static uint32_t tickstart = 0;
+
     //__HAL_GPIO_EXTI_GET_IT returns PR register value by mask TS_INT_PIN
     //this value, shifted to the right by a number of bits equal to the exti line number,
     //will match the ITStatus type
     if ((__HAL_GPIO_EXTI_GET_IT(TS_INT_PIN) >> 13) == SET)
     {
-        //TODO avoid double tapping
         BSP_TS_GetState(&TS_State);
-        em_arg_t out;
-        memset(&out, 0, sizeof(out));
+        if (TS_State.touchDetected && (HAL_GetTick() - tickstart >= DOUBLE_TAP_PAUSE))
+        {
+            em_arg_t out;
+            point_t  point = {.x = TS_State.touchX[0], .y = TS_State.touchY[0]};
 
-        point_t point = {.x = TS_State.touchX[0], .y = TS_State.touchY[0]};
+            out.size = sizeof(point);
+            memcpy(out.data, &point, out.size);
+            em_emit(EM_EVENT_PRESS, &out);
 
-        out.size = sizeof(point);
-        memcpy(out.data, &point, out.size);
-        em_emit(EM_EVENT_PRESS, &out);
-
+            tickstart = HAL_GetTick();
+        }
         __HAL_GPIO_EXTI_CLEAR_IT(TS_INT_PIN);
     }
 }
